e24ce8d92edb01e2ffe85a3f6a116b15a6d717f5
gustavodp22 <90285790+gustavodp22@users.noreply.github.com>
2021-09-07 20:59:28 -0300

    Add files via upload
    """Escriba un algoritmo que lea del teclado un número entero y que compruebe si el número es menor que 10. Si no lo está, debe volver a leer el número repitiendo la operación hasta que el usuario escriba un valor correcto. Finalmente, debe escribir por pantalla el valor leído cuando este sea correcto"""

---
+++ Último commit El programa terminado..py
@@ -0,0 +1,7 @@
+﻿"""Escriba un algoritmo que lea del teclado un número entero y que compruebe si el número es menor que 10. Si no lo está, debe volver a leer el número repitiendo la operación hasta que el usuario escriba un valor correcto. Finalmente, debe escribir por pantalla el valor leído cuando este sea correcto"""
+
+#!/usr/bin/python3
+numero = int(input(u"Ingrese un número"))
+while numero<10:
+ numero = int(input(u"Ingrese un número"))
+print ("numero")
