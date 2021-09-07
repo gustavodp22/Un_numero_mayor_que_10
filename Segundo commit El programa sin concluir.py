4ec4722decc4a54bd2c74a2c37239c4c1a5e7fe6
gustavodp22 <90285790+gustavodp22@users.noreply.github.com>
2021-09-07 20:56:23 -0300

    Add files via upload

---
+++ Segundo commit El programa sin concluir.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+numero -int(input("ingrese un numero mayor a 10:"))
+while (numero <10):
+    numero =int(input("ingrese un numero mayor a 10:"))
+print ( "el numero ingresado es", numero)
